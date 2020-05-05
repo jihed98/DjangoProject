a380b514baf27a9f8eeabba8a3ae170b99b792fd
FilloPrinci <filloprinc98@gmail.com>
2020-05-05 19:18:26 +0200

    Search Bar

--- code/WordCommunity/core/urls.py
+++ code/WordCommunity/core/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', login_required(views.HomeView.as_view()), name='homepage'),
     path('users/', login_required(views.UserList.as_view()), name='user_list'),
     path('user/<username>/', login_required(views.userProfileView), name='user_profile'),
+    path('cerca/', login_required(views.cerca), name='cerca'),
    
 ]
--- code/WordCommunity/core/views.py
+++ code/WordCommunity/core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.models import User
 from django.views.generic.list import ListView
 from forum.models import Articolo
@@ -8,9 +8,24 @@ from forum.models import Articolo
  #   return render(request, 'core/homepage.html')
    
 class HomeView(ListView):
-    queryset = Articolo.objects.all()
+    queryset = Articolo.objects.all().order_by("-pk")
     template_name = 'core/homepage.html'
     context_object_name ="lista_articoli"
+
+
+def cerca(request,):
+    if "q" in request.GET:
+        querystring = request.GET.get("q")
+        print(querystring)
+        if len(querystring) == 0:
+            return redirect("/cerca/")
+        articoli = Articolo.objects.filter(titolo__icontains=querystring)
+        users = User.objects.filter(username__icontains=querystring)
+        print(users)
+        context = {"articoli": articoli, "users": users}
+        return render(request, 'core/cerca.html', context)
+    else:
+        return render(request, 'core/cerca.html')
 
 
 def userProfileView(request, username, ):
