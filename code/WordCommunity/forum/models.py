e8b1646a0972174f57ea3231e980d12ca08b8a99
Andrea Donato <241589@studenti.unimore.it>
2020-05-07 16:46:58 +0200

    prova test

--- code/WordCommunity/forum/models.py
+++ code/WordCommunity/forum/models.py
@@ -2,16 +2,16 @@ from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
 
-class Articolo (models.Model):
+
+class Articolo(models.Model):
     titolo = models.CharField(max_length=120)
     descrizione = models.TextField()
     lingua_sorgente = models.CharField(max_length=5, default="it")
     data_creazione = models.DateTimeField(auto_now_add=True)
     autore_articolo = models.ForeignKey(User, on_delete=models.CASCADE, related_name="articoli")
     indice = models.FloatField(default=0.0)
-    is_listed = models.BooleanField(default=True) # default è pubblico
+    is_listed = models.BooleanField(default=True)  # default è pubblico
 
-    
     def __str__(self):
         return self.titolo
 
