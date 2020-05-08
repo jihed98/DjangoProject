c96ea0c34fbb88ec9c16b1b70ee08ac15838cc2d
Andrea Donato <241589@studenti.unimore.it>
2020-05-08 16:38:42 +0200

    added tests

--- code/WordCommunity/accounts/tests.py
+++ code/WordCommunity/accounts/tests.py
@@ -3,18 +3,17 @@ from django.test import TestCase
 from django.test import Client
 from django.urls import reverse
 
+from accounts.forms import FormRegistrazione
+
 
 class LogInTest(TestCase):
     def setUp(self):
         # Every test needs access to the request factory.
-        #self.factory = RequestFactory()
-        self.dummy_user = User.objects.create_user(username='dummy', password='nothings')
-        self.dummy_user2 = User.objects.create_user(username='dummy2', password='nothing')
+        # self.factory = RequestFactory()
+        self.dummy_user = User.objects.create_user(username='dummy', password='nothings', email='dummy@dummy.com')
+        # self.dummy_user2 = User.objects.create_user(username='dummy2', password='nothing')
 
-    def test_login(self):
-        #dummy_thing = Thing.objects.get(name="Dummy Book")
-        self.client.login(username=self.dummy_user2.username, password='nothing')
-
+    def test_registrazione(self):
         # Test POST invalid data
         response = self.client.post('/accounts/registrazione/', {})
         self.assertFormError(response, 'form', 'username', 'Questo campo è obbligatorio.')
@@ -23,7 +22,22 @@ class LogInTest(TestCase):
         self.assertTemplateUsed(response, 'accounts/registrazione.html')
         self.assertEqual(response.status_code, 200)
 
-        # Test POST valid data?
-        #response = self.client.post('/accounts/create_thing/',
-                                   # {'name': 'new name', 'description': 'new description', })
-        #self.assertEqual(response.status_code, 200)
+        response = self.client.post('/accounts/registrazione/', {'username':self.dummy_user.username, 'email':self.dummy_user.email,
+                                                                 'password1':'vigorsol', 'password2':'wrong'})
+
+        self.assertFormError(response, 'form', 'password2', 'I due campi password non corrispondono.')
+
+        # Test POST valid data
+        # self.client.login(username=self.dummy_user.username, password='vigorsol')
+        form_data = {'username':'user', 'email':'mail','password1':'vigorsol', 'password2':'vigorsol'}
+        form = FormRegistrazione(data=form_data)
+        self.assertTrue(form.is_valid())
+
+    def test_login(self):
+        fake_credential = {'username':'ciao', 'password':'ciao'}
+        true_credential = {'username':'dummy', 'password':'nothings'}
+        t_cred = self.client.login(**true_credential)
+        f_cred = self.client.login(**fake_credential)
+
+        self.assertTrue(t_cred)
+        self.assertFalse(f_cred)
--- code/WordCommunity/core/tests.py
+++ code/WordCommunity/core/tests.py
@@ -1,3 +1,28 @@
+from django.contrib.auth.models import User
+from django.contrib.auth.views import LoginView
 from django.test import TestCase
+from accounts.views import registrazioneView
 
 # Create your tests here.
+from django.urls import reverse
+
+
+class CoreTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='dummy', email='dummy@dummy.com', password='dummypassword')
+        self.credential = {'username':'dummy', 'password':'dummypassword'}
+
+    def test_login_required(self):
+        '''test on login_required'''
+        response = self.client.get('')
+        self.assertRedirects(response,  '/accounts/login/?next=%2F')
+        # 302 --> FOUND: pagina esiste ma non puoi entrarci
+        self.assertEqual(response.status_code, 302)
+
+    def test_homepage(self):
+        self.client.login(**self.credential)
+        response = self.client.get('')
+        self.assertEqual(response.status_code, 200)
+
+
+
