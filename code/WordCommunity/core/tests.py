f2fe443d6eb7807007be20dc85f501aa85378ce5
mcremo95 <m.cremo95@gmail.com>
2020-05-08 17:57:35 +0200

    Aggiunti altri test

--- code/WordCommunity/core/tests.py
+++ code/WordCommunity/core/tests.py
@@ -1,4 +1,7 @@
 from django.contrib.auth.models import User
+
+from core.views import ArticoloChange
+from forum.models import Articolo
 from django.contrib.auth.views import LoginView
 from django.test import TestCase
 from accounts.views import registrazioneView
@@ -11,6 +14,11 @@ class CoreTest(TestCase):
     def setUp(self):
         self.user = User.objects.create_user(username='dummy', email='dummy@dummy.com', password='dummypassword')
         self.credential = {'username':'dummy', 'password':'dummypassword'}
+        self.articolo = Articolo.objects.create(titolo = "Titolo Test",
+                                                descrizione = ['test', 'parola', 'tetto'],
+                                                autore_articolo=self.user)
+        self.user2 = User.objects.create_user(username='dummy2', email='dummy@dummy.com', password='dummypassword')
+        self.credential2 = {'username': 'dummy2', 'password': 'dummypassword'}
 
     def test_login_required(self):
         '''test on login_required'''
@@ -24,5 +32,12 @@ class CoreTest(TestCase):
         response = self.client.get('')
         self.assertEqual(response.status_code, 200)
 
-
-
+    def test_articolo_change(self):
+        self.client.login(**self.credential)
+        response = self.client.get('/articolo/' + str(self.articolo.id) +'/modifica/')
+        self.assertEqual(response.status_code, 200)
+        response = self.client.post('/articolo/' + str(self.articolo.id) +'/modifica/', {})
+        self.assertFormError(response, 'form', 'titolo', 'Questo campo è obbligatorio.')
+        response = self.client.post('/articolo/' + str(self.articolo.id) + '/modifica/',
+                                    {'titolo':'cambio titolo'})
+        self.assertRedirects(response, '/user/' + self.user.username + '/')
