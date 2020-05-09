faafe62f4a75c456a4368cbbfaa79e7ace41e951
mcremo95 <m.cremo95@gmail.com>
2020-05-09 16:00:47 +0200

    Finiti i test sulle view del progetto

--- code/WordCommunity/core/tests.py
+++ code/WordCommunity/core/tests.py
@@ -1,3 +1,5 @@
+import json
+
 from django.contrib.auth.models import User
 
 from core.views import ArticoloChange
@@ -14,8 +16,9 @@ class CoreTest(TestCase):
     def setUp(self):
         self.user = User.objects.create_user(username='dummy', email='dummy@dummy.com', password='dummypassword')
         self.credential = {'username':'dummy', 'password':'dummypassword'}
+        d = {'test':1, 'parola':1, 'tetto':1}
         self.articolo = Articolo.objects.create(titolo = "Titolo Test",
-                                                descrizione = ['test', 'parola', 'tetto'],
+                                                descrizione = json.dumps(d),
                                                 autore_articolo=self.user)
         self.user2 = User.objects.create_user(username='dummy2', email='dummy@dummy.com', password='dummypassword')
         self.credential2 = {'username': 'dummy2', 'password': 'dummypassword'}
@@ -33,6 +36,7 @@ class CoreTest(TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_articolo_change(self):
+        # con utente autenticato
         self.client.login(**self.credential)
         response = self.client.get('/articolo/' + str(self.articolo.id) +'/modifica/')
         self.assertEqual(response.status_code, 200)
@@ -41,3 +45,53 @@ class CoreTest(TestCase):
         response = self.client.post('/articolo/' + str(self.articolo.id) + '/modifica/',
                                     {'titolo':'cambio titolo'})
         self.assertRedirects(response, '/user/' + self.user.username + '/')
+        self.client.logout()
+
+        # con utente non autenticato
+        self.client.login(**self.credential2)
+        response = self.client.get('/articolo/' + str(self.articolo.id) +'/modifica/')
+        self.assertTemplateUsed(response, 'forum/articolo.html')
+        self.assertTemplateNotUsed(response, 'core/modifica.html')
+
+    def test_articolo_delete(self):
+        # con utente autenticato
+        self.client.login(**self.credential)
+        response = self.client.get('/articolo/' + str(self.articolo.id) +'/delete/')
+        self.assertEqual(response.status_code, 200)
+        response = self.client.post('/articolo/' + str(self.articolo.id) + '/delete/', {})
+        self.assertRedirects(response, '/user/' + self.user.username + '/')
+        self.client.logout()
+
+        # con utente non autenticato
+        self.client.login(**self.credential2)
+        response = self.client.get('/articolo/' + str(self.articolo.id) +'/delete/')
+        self.assertTemplateNotUsed(response, 'core/deletearticle.html')
+
+    def test_userList(self):
+        # con utente autenticato
+        self.client.login(**self.credential)
+        response = self.client.get('/users/')
+        self.assertTemplateUsed(response, 'core/users.html')
+        self.client.logout()
+
+        # con utente autenticato
+        response = self.client.get('/users/')
+        self.assertTemplateNotUsed(response, 'core/users.html')
+
+    def test_userProfileView(self):
+        # con utente autenticato
+        # su il tuo profilo
+        self.client.login(**self.credential)
+        response = self.client.get('/user/' + self.user.username + '/')
+        self.assertTemplateUsed(response, 'core/profilo.html')
+        self.assertTrue(response.status_code, 200)
+
+        # sul profilo degli altri
+        response = self.client.get('/user/' + self.user2.username + '/')
+        self.assertTemplateNotUsed(response, 'core/profilo.html')
+        self.assertTemplateUsed(response, 'core/user_profile.html')
+
+        # con utente non autenticato
+        self.client.logout()
+        response = self.client.get('/user/' + self.user.username + '/')
+        self.assertRedirects(response, '/accounts/login/?next=/user/' + self.user.username + '/')
