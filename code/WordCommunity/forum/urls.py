690abd228da71ff623e39bfb29baa1222e8cf071
jihed <jihed.fatnassi@capgemini.com>
2020-05-19 13:19:55 +0200

    Delete All

--- code/WordCommunity/forum/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-from django.contrib.auth.decorators import login_required
-
-urlpatterns = [
-   path('nuovo-articolo/', login_required(views.CreaArticolo.as_view()), name="crea_articolo"),
-   path('articolo/<int:pk>/', login_required(views.visualizzaArticolo), name="articolo_view"),
-  ]
